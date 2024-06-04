86bb62f947bdef8105719119535b426bd59fc42d
Andre <dtandre331@gmail.com>
2024-06-04 16:27:09 -0400

    fix: change comments in utils.go

--- lexer/utils.go
+++ lexer/utils.go
@@ -2,17 +2,17 @@ package lexer
 
 import "github.com/andrearcaina/bamboo/token"
 
-// IsDigit checks if a character is a number
+// isNumber checks if a character is a number
 func isNumber(ch byte) bool {
 	return '0' <= ch && ch <= '9' || ch == '.'
 }
 
-// IsLetter checks if a character is a letter
+// isLetter checks if a character is a letter
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
-// NewToken creates a new token with a type and a literal value
+// newToken creates a new token with a type and a literal value
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
